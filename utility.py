f9d14241041d4f972fc40f40f7969881fe197047
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-01-20 23:09:40 +0600

    value 0-1 returned

--- utility.py
+++ utility.py
@@ -1,23 +1,35 @@
-def utility(state, x, y, pcTurn):
-  
-  patternComputer = [ "XXXXX", "-XXXX-", "XXXX-", "XX-XX", "XXX-X", "X-XXX-", "-XXX-", "XXX--", "-XX-X",
-			"XX-X-", "-XX-X-", "--XX-", "XX---", "X---X", "X----", "--X--", "-X---" ];
-  
-  patternMan = [ "OOOOO", "-OOOO-", "OOOO-", "OO-OO", "OOO-O", "O-OOO-", "-OOO-", "OOO--", "-OO-O",
-			"OO-O-", "-OO-O-", "--OO-", "OO---", "O---O", "O----", "--O--", "-O---" ];
-  
+def utility(gameState, x, y):
 
-  profit = [ 200000000, 50000, 15000, 420, 420, 480, 350, 300, 100, 100, 100, 50 / 15, 50 / 15, 50 / 15,
-			20 / 10, 20 / 10, 20 / 10 ];
+  #-XXXX- 2 50000  X-XXX- 6 480 "-XX-X-" 11 100
+  patternComputer = [ "XXXXX", "XXXX-", "XX-XX", "XXX-X", "-XXX-", "XXX--", "-XX-X", #7
+			"XX-X-", "--XX-", '-XX--', "XX---", 'XX--X', 'X-X--', "X---X",  'X--X-', '-X-X-', 'X-X-X', "--X--", "-X---", "X----" ]; #14
+  
+  patternMan = [ "OOOOO", "OOOO-", "OO-OO", "OOO-O", "-OOO-", "OOO--", "-OO-O", #7
+			"OO-OO-", "--OO-", '-OO--', "OO---", 'OO--O', 'O-O--', "O---O",  'O--O-', '-O-O-', 'O-O-O', "--O--", "-O---", "O----" ];
+  
+  
+  profit = [ 200000000, 50000, 15000, 15000, 480, 420, 350, 350, 200, 150, 50, 20, 10, 10, 10, 10, 10, 5, 5, 1 ]
+  #sum = 200082081
+  state = [[]]
+  for i in range(10):
+    for j in range(10):
+      if gameState[i][j] == 1:
+        state[i].append('X')
+      elif gameState[i][j] == -1:
+        state[i].append('O')
+      else:
+        state[i].append('-')
 
-  if pcTurn:
-    #state[x][y] = 'X'
+
+  if state[x][y]== 'X':
+    
+    state[x][y] = '-'
     mult = 1 #pcTurn * 1 + (1-pcTurn) * -1
-    pcProfit = []
+    pcProfit = 0
     #diagonal right left
     for iter in range(5):
-      pr = x + iter //r for right diagonal
-      qr = y - iter //l for left diagonal
+      pr = x + iter #r for right diagonal
+      qr = y - iter #l for left diagonal
       pl = x + iter
       ql = y + iter
       sr = ''
@@ -36,41 +48,41 @@ def utility(state, x, y, pcTurn):
       
       if sr in patternMan:
         indx = patternMan.index(sr)
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
       
-      if sr[::-1] in patternMan:
+      elif sr[::-1] in patternMan:
         indx = patternMan.index(sr[::-1])
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
       
       else:
         sr2 = sr[:iter] + 'X' + sr[iter+1:]
         
         if sr2 in patternComputer:
           indx = patternComputer.index(sr2)
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
         
         elif sr2[::-1] in patternComputer:
           indx = patternComputer.index(sr2[::-1])
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
 
       if sl in patternMan:
         indx = patternMan.index(sl)
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
       
       elif sl[::-1] in patternMan:
         indx = patternMan.index(sl[::-1])
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
 
       else:
         sl2 = sl[:iter] + 'X' + sl[iter+1:]
         
         if sl2 in patternComputer:
           indx = patternComputer.index(sl2)
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
         
         elif sl2[::-1] in patternComputer:
           indx = patternComputer.index(sl2[::-1])
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
 
     #up down left right
     for iter in range(5):
@@ -93,54 +105,54 @@ def utility(state, x, y, pcTurn):
       
       if su in patternMan:
         indx = patternMan.index(su)
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
       
       elif su[::-1] in patternMan:
         indx = patternMan.index(su[::-1])
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
 
       else:
         su2 = su[:iter] + 'X' + su[iter+1:]
         
         if su2 in patternComputer:
           indx = patternComputer.index(su2)
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
         
-        if su2[::-1] in patternComputer:
+        elif su2[::-1] in patternComputer:
           indx = patternComputer.index(su2[::-1])
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
 
       if sh in patternMan:
         indx = patternMan.index(sh)
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
 
       elif sh[::-1] in patternMan:
         indx = patternMan.index(sh[::-1])
-        pcProfit += [mult * profit[indx]]
+        pcProfit += mult * profit[indx]
       
       else:
         sh2 = sh[:iter] + 'X' + sh[iter+1:]
         
         if sh2 in patternComputer:
           indx = patternComputer.index(sh2)
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
 
         elif sh2[::-1] in patternComputer:
           indx = patternComputer.index(sh2[::-1])
-          pcProfit += [mult * profit[indx]]
+          pcProfit += mult * profit[indx]
     
-    return pcProfit
+    return pcProfit / 290082081
 
 
     else:
       
-      #state[x][y] = 'X'
+      state[x][y] = '-'
       mult = -1 #pcTurn * 1 + (1-pcTurn) * -1
-      manProfit = []
+      manProfit = 0
       #diagonal right left
       for iter in range(5):
-        pr = x + iter //r for right diagonal
-        qr = y - iter //l for left diagonal
+        pr = x + iter #r for right diagonal
+        qr = y - iter #l for left diagonal
         pl = x + iter
         ql = y + iter
         sr = ''
@@ -159,41 +171,41 @@ def utility(state, x, y, pcTurn):
         
         if sr in patternComputer:
           indx = patternComputer.index(sr)
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
         
-        if sr[::-1] in patternComputer:
+        elif sr[::-1] in patternComputer:
           indx = patternComputer.index(sr[::-1])
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
         
         else:
           sr2 = sr[:iter] + 'O' + sr[iter+1:]
           
           if sr2 in patternMan:
             indx = patternMan.index(sr2)
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
           
           elif sr2[::-1] in patternMan:
             indx = patternMan.index(sr2[::-1])
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
 
         if sl in patternComputer:
           indx = patternComputer.index(sl)
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
         
         elif sl[::-1] in patternComputer:
           indx = patternComputer.index(sl[::-1])
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
 
         else:
           sl2 = sl[:iter] + 'O' + sl[iter+1:]
           
           if sl2 in patternMan:
             indx = patternMan.index(sl2)
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
           
           elif sl2[::-1] in patternMan:
             indx = patternMan.index(sl2[::-1])
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
 
       #up down left right
       for iter in range(5):
@@ -216,40 +228,40 @@ def utility(state, x, y, pcTurn):
         
         if su in patternComputer:
           indx = patternComputer.index(su)
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
         
         elif su[::-1] in patternComputer:
           indx = patternComputer.index(su[::-1])
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
 
         else:
           su2 = su[:iter] + 'O' + su[iter+1:]
           
           if su2 in patternMan:
             indx = patternMan.index(su2)
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
           
-          if su2[::-1] in patternMan:
+          elif su2[::-1] in patternMan:
             indx = patternMan.index(su2[::-1])
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
 
         if sh in patternComputer:
           indx = patternComputer.index(sh)
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
 
         elif sh[::-1] in patternComputer:
           indx = patternComputer.index(sh[::-1])
-          manProfit += [mult * profit[indx]]
+          manProfit += mult * profit[indx]
         
         else:
           sh2 = sh[:iter] + 'O' + sh[iter+1:]
           
           if sh2 in patternMan:
             indx = patternMan.index(sh2)
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
 
           elif sh2[::-1] in patternMan:
             indx = patternMan.index(sh2[::-1])
-            manProfit += [mult * profit[indx]]
+            manProfit += mult * profit[indx]
       
-      return manProfit
+      return manProfit / 290082081
